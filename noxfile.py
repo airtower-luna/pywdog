b5690bf2f01cdb296114e24aa389b71157ec397d
Fiona Klute <fiona.klute@gmx.de>
2025-04-01 20:55:14 +0200

    Record test coverage for Python code

--- noxfile.py
+++ noxfile.py
@@ -8,4 +8,16 @@ wrapper = Path(__file__).parent / 'test-namespace-wrapper.sh'
 def test(session):
     """Run tests."""
     session.install('.[tests]')
-    session.run('sh', str(wrapper), 'pytest', '-v', external=True)
+    session.run(
+        'sh', str(wrapper),
+        'pytest', '--cov=wdog', '--cov-report=term', '-v',
+        env={'COVERAGE_FILE': f'.coverage.{session.python}'}, external=True)
+    session.notify('coverage')
+
+
+@nox.session
+def coverage(session):
+    """Generate combined coverage report."""
+    session.install('coverage')
+    session.run('coverage', 'combine')
+    session.run('coverage', 'html')
