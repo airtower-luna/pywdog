1769c1ba3aff594b31a584b0211c4d09ee230ceb
Fiona Klute <fiona.klute@gmx.de>
2025-04-04 00:20:51 +0200

    Run tests using watchdogd from subproject, if built
    This allows running the tests without installing watchdogd or manual
    configuration.

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,28 @@
+import os
 import nox
 from pathlib import Path
 
 wrapper = Path(__file__).parent / 'test-namespace-wrapper.sh'
+build_dir = Path(__file__).parent / 'noxbuild'
 
 
 @nox.session(python=['3.13'])
 def test(session):
     """Run tests."""
-    session.install('.[tests]')
+    session.install('.[tests]', f'--config-settings=build-dir={build_dir!s}')
+    testenv = {
+        'COVERAGE_FILE': f'.coverage.{session.python}'
+    }
+    # If watchdogd was built as subproject, use it unless there's an
+    # explicit override.
+    watchdogd_internal = build_dir \
+        / 'subprojects/watchdogd/dist/usr/local/sbin/watchdogd'
+    if watchdogd_internal.is_file() and 'TEST_WATCHDOGD' not in os.environ:
+        testenv['TEST_WATCHDOGD'] = str(watchdogd_internal)
     session.run(
         'sh', str(wrapper),
         'pytest', '--cov=wdog', '--cov-report=term', '-v',
-        env={'COVERAGE_FILE': f'.coverage.{session.python}'}, external=True)
+        env=testenv, external=True)
     session.notify('coverage')
 
 
